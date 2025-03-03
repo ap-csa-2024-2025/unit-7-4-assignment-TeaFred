871e26a448021219baff09c3242cab189384cf4e
TeaFred <fhtruong100@student.hbuhsd.edu>
2025-03-03 07:42:34 +0000

    easzsy done

--- Main.java
+++ Main.java
@@ -6,13 +6,18 @@ public class Main
   public static void main(String[] args)
   {
     Scanner sc = new Scanner(System.in);
-    ArrayList<String> input = new ArrayList<String>();
+    ArrayList<String> theArrayList = new ArrayList<String>();
 
     String word = "";
     while (!word.equals("STOP"))
     {
-      System.out.println("Implement me!");
+      System.out.println("You are in the loop, please enter a word! ");
+      word = sc.nextLine();
+      theArrayList.add(word);
     }
+    theArrayList.remove(theArrayList.size()-1);
+    //System.out.println(countSecondInitial(theArrayList, "i"));
+    System.out.println(searchSecond(theArrayList));
   }
 
   /** Returns the number of Strings in the ArrayList that has the letter
@@ -20,17 +25,44 @@ public class Main
    * Precondition: The arr is not null, and is not empty.
    * Precondition: The letter is exactly one letter.
    */
-  public static int countSecondInitial(ArrayList<String> arr, String letter)
+  public static int countSecondInitial(ArrayList<String> theArrayListString, String target)
   {
-    return 0;
+    int theCounter = 0;
+    for (int i = 0; i < theArrayListString.size(); i++)
+    {
+      if ((theArrayListString.get(i).indexOf(target) == 1)) 
+      {
+        theCounter++;
+      }
+    }
+    System.out.print("Search for i: ");
+    return theCounter;
   }
 
   /** Returns the index of the second occurrence of target within the ArrayList
    * If the target is not found, or is only found once, then -1 is returned.
    * Precondition: Neither arr nor target are null.  The arr is non-empty.
    */
-  public static int searchSecond(final ArrayList<String> arr, String target)
+  public static int searchSecond(final ArrayList<String> theArrayList)
   {
-    return -1;
+    Scanner sc = new Scanner(System.in);
+    String target; 
+    System.out.println("Enter String to search for. ");
+    target = sc.nextLine();
+    int theIndex = -1;
+    int theCount = 0;
+    for (int i = 0; i < theArrayList.size(); i++)
+    {
+      if (theArrayList.get(i).equals(target))
+      {
+        theCount++; 
+      }
+      if (theCount == 2)
+      {
+        theIndex = i;
+      }
+    }
+    System.out.print("searchSecond returns: ");
+    return theIndex; 
   }
 }
